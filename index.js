f50b59b9ef537f09fa754c222a2159c2b55ee643
miyanipritesh <miyanipritesh1018@gmail.com>
2025-07-04 16:12:30 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/api/message', (req, res) => {
+  res.json({
+    status: "success",
+    message: "Hello! This is your API response."
+  });
+});
+
+app.listen(port, () => {
+  console.log(`API is running on http://localhost:${port}`);
+});
